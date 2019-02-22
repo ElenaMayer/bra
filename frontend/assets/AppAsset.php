dec6acca130763501efdd0f1036c23f7b33d1d2f
Elena Mayer <elena.mayer@rebelmouse.com>
2019-02-22 12:05:28 +0700

    Mobile index fix2

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -25,7 +25,7 @@ class AppAsset extends AssetBundle
         'css/magnific-popup.css',
         'css/font-icons.css',
         'css/sliders.css',
-        'css/style.css?88',
+        'css/style.css?93',
         'css/animate.min.css',
     ];
     public $js = [
