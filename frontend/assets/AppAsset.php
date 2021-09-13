db49f2680b59ef073c8316545f6a91d8873a4ea6
Mayer Elena <mea@terrasol.biz>
2021-09-13 11:16:11 +0700

    Add colors

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -25,7 +25,7 @@ class AppAsset extends AssetBundle
         'css/magnific-popup.css',
         'css/font-icons.css?2',
         'css/sliders.css',
-        'css/style.css?118',
+        'css/style.css?119',
         'css/animate.min.css',
     ];
     public $js = [
